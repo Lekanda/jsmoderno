30ccb00dd952a972cc1b5e396672bd9eed0315ce
lekanda <abernaolao@gmail.com>
2022-02-07 13:43:45 +0100

    console en JS.

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -1,1 +1,7 @@
-console.log('Hola mundo');
+console.time('Hola'); // 
+console.warn('Algo salio mal');
+console.warn('Algo salio mal');
+console.warn('Algo salio mal');
+console.warn('Algo salio mal');
+console.warn('Algo salio mal');
+console.timeEnd('Hola'); // Da le tiempo que tarda en ejecutar, con el de arriba
