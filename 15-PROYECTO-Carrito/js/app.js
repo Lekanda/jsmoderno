5cacda866530dece887fd7f96b3e038912664d82
lekanda <abernaolao@gmail.com>
2022-04-24 15:24:09 +0200

    Leer los datos del curso que seleccionamos.

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -15,6 +15,22 @@ function cargarEventListeners () {
 function agregarCurso(e){
     e.preventDefault();
     if(e.target.classList.contains('agregar-carrito')){
-        console.log(e.target);
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosCurso(cursoSeleccionado);
     }
+}
+
+// Lee el contenido del HTML al que le damos click y extre la info del curso(cursos)
+function leerDatosCurso(curso){
+    console.log(curso);
+
+    // Crear un objeto con el contenido del curso seleccionado
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    }
+    console.log(infoCurso);
 }
