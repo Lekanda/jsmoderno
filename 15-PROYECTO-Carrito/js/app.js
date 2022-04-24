2da507fab14a88ddd04933e6bba5a2cedbdd9072
lekanda <abernaolao@gmail.com>
2022-04-24 21:06:14 +0200

    Mostrando en el carrito el curso seleccionado.

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -4,6 +4,11 @@ const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 const listaCursos = document.querySelector('#lista-cursos');
 
+let articulosCarrito = [];
+
+
+
+
 cargarEventListeners();
 function cargarEventListeners () {
     // Cuando agregas un curso presionando 'agregar al carrito'
@@ -22,7 +27,7 @@ function agregarCurso(e){
 
 // Lee el contenido del HTML al que le damos click y extre la info del curso(cursos)
 function leerDatosCurso(curso){
-    console.log(curso);
+    // console.log(curso);
 
     // Crear un objeto con el contenido del curso seleccionado
     const infoCurso = {
@@ -32,5 +37,37 @@ function leerDatosCurso(curso){
         id: curso.querySelector('a').getAttribute('data-id'),
         cantidad: 1
     }
-    console.log(infoCurso);
+
+    // Agrega elementos al arreglo de cursos del carrito
+    articulosCarrito = [...articulosCarrito, infoCurso];
+    // console.log(articulosCarrito);
+    carritoHTML();
+}
+
+// Muestra el Carrito de Compras en el HTML
+function carritoHTML(){
+    // Limpiar el HTML del carrito
+    limpiarHTML();
+
+    // Recorre el carrito y genera el HTML
+    articulosCarrito.forEach( curso => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>
+                ${curso.titulo}
+            </td>
+        `;
+        
+        //Agrega el HTML del carrito en el tbody
+        contenedorCarrito.appendChild(row);
+    });
+}
+
+// Elimina los cursos del carrito , del tbody.
+function limpiarHTML(params) {
+    // contenedorCarrito.innerHTML = '';
+// while para eliminar todos los elementos del tbody
+    while (contenedorCarrito.firstChild) {  
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+    }
 }
